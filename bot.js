8aad1050506904ea400d7bb5fc67236fe35e7082
GameFozerYT <48291236+GameFozerYT@users.noreply.github.com>
2019-04-06 19:11:25 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : Texo`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Texo ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Im The King`,"http://twitch.tv/King")
+client.user.setStatus("dnd")
+});
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
